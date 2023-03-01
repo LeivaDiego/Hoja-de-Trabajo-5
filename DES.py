82dd1592043f72c981629505b5a2306ab697086b
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-28 18:53:52 -0600

    Version 3.0 Evento "NEW"
    Simulacion del evento NEW agregada.
    Mejoras en la documentacion del programa

--- DES.py
+++ DES.py
@@ -2,35 +2,48 @@
 Programa de simulación DES (Discrete Event Simulation)
 usando el módulo SimPy.
 
-Autores:    Diego Leiva
+Autores: Diego Leiva
             Pablo Orellana
+
+Referencia de ejemplos de Malonso-UVG
 '''
 
 import random as rnd
 import simpy as smp
 
 #Parametro de Simulacion
-Random_Seed = 2912
-Process_Qty = 25
-Interval = 5
-RAM_Capacity = 100
-CPU_Cores = 1
+Random_Seed = 2912      #Semilla para la generacion de numeros al azar
+Process_Qty = 25        #Cantidad de procesos a simular
+Interval = 10           #Intervalo para la creacion de procesos
+RAM_Capacity = 100      #Cantidad de Memoria RAM de la computadora
+CPU_Cores = 1           #Cantidad de procesadores
 
 #Informacion de simulacion
-Time_of_Execution = 0
-Time_List = []
+Time_of_Execution = 0   #Tiempo que tomo la ejecucion de un proceso
+Time_List = []          #Listado con los tiempos de ejecucion de los procesos simulados
+
 
 def ProcessBuilder():
     print("xd")
 
+'''Instancia de metodo encargado de realizar los diferentes eventos para un proceso'''
 def Process(env,id,cpu,ram):
-    Start_Time = 0.0
-    Finish_Time = 0.0
-    Process_Completed = False
-    Memory_Qty = rnd.randint(1,10)
-    Intructions_Qty = rnd.randint(1,10)
 
+    #Condiciones iniciales del proceso
+    Start_Time = 0.0                        #Tiempo de la simulacion en que inicio el proceso
+    Finish_Time = 0.0                       #Tiempo de la simulacion en que se termino el proceso
+    Process_Completed = False               #Si el proceso ha terminado o no
+    Memory_Qty = rnd.randint(1,10)          #Cantidad de memoria que el proceso requiere
+    Intructions_Qty = rnd.randint(1,10)     #Cantidad de instrucciones a realizar
 
+    #Evento NEW, cuando el proceso llega al sistema
+    print("NEW")
+    print(f'Proceso: {id}')
+    print(f'Inicializado en: {env.now}')
+    print(f'Cantidad de memoria utilizada: {Memory_Qty}\n')
+    Start_Time = env.now
+
+   
 
 
 #Simulacion
