07a0bfffda19181d9a25118ae2cd1943cc78cf01
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-28 19:04:06 -0600

    Version 4.0 Evento "READY"
    Simulacion del evento READY agregada.
    Mejoras en la documentacion del programa

--- DES.py
+++ DES.py
@@ -1,4 +1,4 @@
-'''
+"""
 Programa de simulación DES (Discrete Event Simulation)
 usando el módulo SimPy.
 
@@ -6,48 +6,61 @@ Autores: Diego Leiva
             Pablo Orellana
 
 Referencia de ejemplos de Malonso-UVG
-'''
+"""
 
 import random as rnd
 import simpy as smp
 
-#Parametro de Simulacion
-Random_Seed = 2912      #Semilla para la generacion de numeros al azar
-Process_Qty = 25        #Cantidad de procesos a simular
-Interval = 10           #Intervalo para la creacion de procesos
-RAM_Capacity = 100      #Cantidad de Memoria RAM de la computadora
-CPU_Cores = 1           #Cantidad de procesadores
+# Parámetro de Simulación
+Random_Seed = 2912  # Semilla para la generación de números al azar
+Process_Qty = 25  # Cantidad de procesos a simular
+Interval = 10  # Intervalo para la creación de procesos
+RAM_Capacity = 100  # Cantidad de Memoria RAM de la computadora
+CPU_Cores = 1  # Cantidad de procesadores
 
-#Informacion de simulacion
-Time_of_Execution = 0   #Tiempo que tomo la ejecucion de un proceso
-Time_List = []          #Listado con los tiempos de ejecucion de los procesos simulados
+# Información de simulación
+Time_of_Execution = 0  # Tiempo que tomo la ejecución de un proceso
+Time_List = []  # Listado con los tiempos de ejecución de los procesos simulados
 
 
-def ProcessBuilder():
+def process_builder():
     print("xd")
 
-'''Instancia de metodo encargado de realizar los diferentes eventos para un proceso'''
-def Process(env,id,cpu,ram):
 
-    #Condiciones iniciales del proceso
-    Start_Time = 0.0                        #Tiempo de la simulacion en que inicio el proceso
-    Finish_Time = 0.0                       #Tiempo de la simulacion en que se termino el proceso
-    Process_Completed = False               #Si el proceso ha terminado o no
-    Memory_Qty = rnd.randint(1,10)          #Cantidad de memoria que el proceso requiere
-    Intructions_Qty = rnd.randint(1,10)     #Cantidad de instrucciones a realizar
+'''Instancia de método encargado de realizar los diferentes eventos para un proceso'''
 
-    #Evento NEW, cuando el proceso llega al sistema
+
+def process_sim(env, id, cpu, ram):
+    # Condiciones iniciales del proceso
+    start_time = 0.0  # Tiempo de la simulación en que inicio el proceso
+    finish_time = 0.0  # Tiempo de la simulación en que se terminó el proceso
+    process_completed = False  # Si el proceso ha terminado o no
+    memory_qty = rnd.randint(1, 10)  # Cantidad de memoria que el proceso requiere
+    instructions_qty = rnd.randint(1, 10)  # Cantidad de instrucciones a realizar
+
+    # Evento NEW, cuando el proceso llega al sistema
     print("NEW")
     print(f'Proceso: {id}')
     print(f'Inicializado en: {env.now}')
-    print(f'Cantidad de memoria utilizada: {Memory_Qty}\n')
-    Start_Time = env.now
+    print(f'Cantidad de memoria utilizada: {memory_qty}\n')
+    start_time = env.now
 
-   
+    # Ciclo que permite realizar las instrucciones del proceso hasta que este termine
+    while not process_completed:
+        with cpu.request() as req:
+            yield req
+            yield env.timeout(1)
+            yield ram.get(memory_qty)
+
+            # Evento READY, cuando el proceso está listo para correr
+            print("READY")
+            print(f'Proceso: {id}')
+            print(f'Listo para ejecutarse en: {env.now}')
+            print(f'Cantidad de Instrucciones: {instructions_qty}\n')
 
 
-#Simulacion
+# Simulación
 rnd.seed(Random_Seed)
-env = smp.Environment()
-cpu = smp.Resource(env, capacity=CPU_Cores)
-ram = smp.Container(env, init=RAM_Capacity,capacity=RAM_Capacity)
+environment = smp.Environment()
+CPU = smp.Resource(environment, capacity=CPU_Cores)
+RAM = smp.Container(environment, init=RAM_Capacity, capacity=RAM_Capacity)
